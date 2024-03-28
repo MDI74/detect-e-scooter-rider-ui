0c930ca0f20b9ae096ddddba834732b1547bf395
Dmitriy Myakotin <demon4301@gmail.com>
2024-03-28 20:43:14 +0500

    feat: add header component

---
+++ src/components/Header/Header.cy.tsx
@@ -0,0 +1,25 @@
+import { Header } from './Header';
+
+describe('Header', () => {
+  it(`
+  GIVEN main page
+  WHEN visit main page
+  THEN render header component
+  `, () => {
+    mountComponent();
+    cy.get('.header')
+      .should('exist');
+
+    cy.get('.header__wrapper')
+      .should('exist');
+
+    cy.get('.header__title')
+      .should('have.text', 'E-SCOOTER RIDER DETECTER');
+  });
+});
+
+function mountComponent() {
+  cy.mount(
+    <Header />,
+  );
+}
